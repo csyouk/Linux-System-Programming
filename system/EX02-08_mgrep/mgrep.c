a075fb74368764f717388a3e191e202291b40fdc
csyouk <csyouk@hanmail.net>
2017-11-08 16:52:07 +0900

    update documents

--- system/EX02-08_mgrep/mgrep.c
+++ system/EX02-08_mgrep/mgrep.c
@@ -22,19 +22,28 @@ int main(int argc, char **argv)
 	num_of_child = argc - 2;
 
 	/* Implement code */
-	/* Implement code */
 	for(i=0; i < num_of_child;i++){
 		pid_temp = fork();
-		if(pid_temp == 0){
+		if(pid_temp == -1){
+			printf("[%d] error : %s (%d)\n", pid, strerror(errno), __LINE__);
+			return EXIT_FAILURE;
+		}
+		else if(pid_temp == 0){
 			printf("about to run [grep -rn %s /etc]\n", argv[2+i]);
-			execlp("grep", "grep", "-rn", argv[2+i], "/etc", NULL);
-		} else {
-			int status;
-			pid_wait = wait(&status);
-			printf("pid [%d] has been terminted with status %d\n", pid_wait, status);
+			execlp("grep", "grep", "-rn", argv[2+i], argv[1], NULL);
+			printf("=============="); // exec가 제대로 프로세스를 교체했다면, 이 실행문은 실행되지 않는다.
 		}
 	}
-	printf("[%d] terminted\n", pid);
+
+	for(i=0; i < num_of_child; i++){
+		int status;
+		printf("[%d] waiting child's termination\n", pid);
+		pid_wait = wait(&status);
+		printf("[%d] pid %d has been terminated with status %#x\n", pid, pid_wait, status);
+	}
+
+	printf("[%d] terminated\n", pid);
+
 
 	return EXIT_SUCCESS;
 }
